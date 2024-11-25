b81279690a8797f25892c943c9cf727660a93a91
bluesatch <satchwerk@gmail.com>
2024-11-25 11:51:55 -0600

    loaded items

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ class MenuForm {
         this.dessertsRow = document.getElementById('dessertsRow')
         this.drinksRow = document.getElementById('drinksRow')
         this.foodList = document.getElementById('foodList')
+
         // create array of food items
         this.menu = [
             {
@@ -21,112 +22,128 @@ class MenuForm {
                 type: 'meat',
                 item: 'fried turkey',
                 imgUrl: 'fried_turkey.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Satch'
             },
             {
                 id: 2,
                 type: 'meat',
                 item: 'oven turkey',
                 imgUrl: 'oven_turey.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Mom'
             },
             {
                 id: 3,
                 type: 'meat',
                 item: 'ham',
                 imgUrl: 'ham.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Cousin Tee'
             },
             {
                 id: 4,
                 type: 'meat',
                 item: 'turducken',
                 imgUrl: 'turducken.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Aunt Bea'
             },
             {
                 id: 5, 
                 type: 'sides',
                 item: 'dressing',
                 imgUrl: 'dressing.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Granny'
             }, 
             {
                 id: 6,
                 type: 'sides',
                 item: 'turnip greens',
                 imgUrl: 'turnip_greens.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Cousin Gee'
             },
             {
                 id: 7,
                 type: 'sides',
                 item: 'mashed potatoes w/gravy',
                 imgUrl: 'mashed_potatoes.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Aunt Kay'
             },
             {
                 id: 8,
                 type: 'sides',
                 item: 'deep fried loaded baked potato balls',
                 imgUrl: 'baked_potato_balls.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Sister Q'
             },
             {
                 id: 9,
                 type: 'sides',
                 item: "mac n' cheese",
                 imgUrl: 'macncheese.jpeg',
-                isChecked: false 
+                isChecked: false,
+                madeBy: 'Aunt V' 
             },
             {
                 id: 10,
                 type: 'sides',
                 item: 'cranberry sauce',
                 imgUrl: 'cranberry_sauce.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Brother T'
             },
             {
                 id: 11, 
                 type: 'sides',
                 item: 'greenbean casserole',
                 imgUrl: 'greenbean_casserole.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: "Brother T's wife"
             },
             {
                 id: 12,
                 type: 'sides',
                 item: 'cornbread',
                 imgUrl: 'cornbread.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: "Cousin Gee's husband"
             },
             {
                 id: 13, 
                 type: 'desserts',
                 item: 'sweet potato pie',
                 imgUrl: 'sweet_potato_pie.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: "Granny"
             },
             {
                 id: 14,
                 type: 'desserts',
                 item: 'pecan pie',
                 imgUrl: 'pecan_pie.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Mom'
             },
             {
                 id: 15, 
                 type: 'desserts',
                 item: 'pound cake',
                 imgUrl: 'pound_cake.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: 'Satch'
             },
             {
                 id: 16, 
                 type: 'desserts',
                 item: 'bread pudding',
                 imgUrl: 'bread_pudding.jpeg',
-                isChecked: false
+                isChecked: false,
+                madeBy: "Sister T"
             },
             {
                 id: 17,
@@ -160,10 +177,71 @@ class MenuForm {
     }
 
     init() {
-        console.log('initialized')
+        // console.log('initialized')
+        this.buildFigures(this.menu)
     }
 
-    
+    loadItems(el, child) {
+        // console.log('loaded')
+        el.appendChild(child)
+    }
+
+    buildFigures(arr) {
+
+        arr.forEach(obj => {
+            // for each object build Figure
+            const column = document.createElement('div')
+            column.classList.add('col')
+
+            column.innerHTML = `
+                <div class="figure-div">
+                    <figure class="figure item-figure">
+                        <img src="https://via.placeholder.com/200x200" alt="placeholder img" class="img-fluid image figure-img food-image rounded" />
+                        <figcaption class="figure-caption food-caption">${obj.madeBy}</figcaption>
+                    </figure>
+                    <h3 class="food-heading">${obj.item}</h3>
+                    <div class="form-check">
+                        <input 
+                            type="checkbox" 
+                            name="${obj.type}" 
+                            id="${obj.type}-${obj.id}"
+                            value="${obj.item}"
+                            class="form-check-input"
+                        />
+                        <label 
+                            for="${obj.type}-${obj.id}" 
+                            class="text-capitalize form-check-label">
+                            ${obj.item}
+                        </label>
+                    </div>
+                </div>
+            `
+
+            // console.log(column)
+
+            // this.rows.forEach(row => {
+            //     this.loadItems(row, column)
+            // })
+            switch (obj.type) {
+                case 'meat': 
+                    this.loadItems(this.meatRow, column)
+                    break
+                case 'sides':
+                    this.loadItems(this.sidesRow, column)
+                    break
+                case 'desserts':
+                    this.loadItems(this.dessertsRow, column)
+                    break 
+                case 'drinks':
+                    this.loadItems(this.drinksRow, column)
+                    break 
+                default: 
+                    return 'error'
+            }
+        })
+    }
+
+
 }
 
 const action = new MenuForm() 
